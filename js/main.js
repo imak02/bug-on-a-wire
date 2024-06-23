53f8f09bbadce4d5b1d31493729907ddfe55b367
Asbin Khanal <69668769+imak02@users.noreply.github.com>
2024-06-23 22:14:04 +0545

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,20 +9,20 @@ document.getElementById("play-button").addEventListener("click", function () {
   canvas.height = window.innerHeight;
 
   const logImage = new Image();
-  logImage.src = "../assets/img/log.png";
+  logImage.src = "./../assets/img/log.png";
 
   const insectSprite = new Image();
-  insectSprite.src = "../assets/img/insectSprite.png";
+  insectSprite.src = "./../assets/img/insectSprite.png";
 
   const poleImage = new Image();
-  poleImage.src = "../assets/img/pole.png";
+  poleImage.src = "./../assets/img/pole.png";
 
   const chickenSprite = new Image();
-  chickenSprite.src = "../assets/img/chickenSprite.png";
+  chickenSprite.src = "./../assets/img/chickenSprite.png";
 
-  const backgroundMusic = new Audio("../assets/sounds/background.mp3");
-  const moveSound = new Audio("../assets/sounds/move.wav");
-  const collisionSound = new Audio("../assets/sounds/collision.wav");
+  const backgroundMusic = new Audio("./../assets/sounds/background.mp3");
+  const moveSound = new Audio("./../assets/sounds/move.wav");
+  const collisionSound = new Audio("./../assets/sounds/collision.wav");
 
   let imagesLoaded = 0;
 
