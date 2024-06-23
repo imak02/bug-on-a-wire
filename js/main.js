8036c343115aea5afd6cd2ed8da979803eccd7d8
imak02 <asbinkhanal222@gmail.com>
2024-06-23 19:42:12 +0545

    feat: add initial game screen ui

---
+++ js/main.js
@@ -0,0 +1,4 @@
+document.getElementById("play-button").addEventListener("click", function () {
+  document.getElementById("menu").style.display = "none";
+  document.getElementById("main-game").style.display = "block";
+});
